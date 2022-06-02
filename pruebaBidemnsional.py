69d19bac2a022ab330a090eca74a8ace60cf3c8e
yaravivillegas <yaravi.villegas@gmail.com>
2022-06-02 13:44:24 -0400

    declarar arreglo bidimensional

---
+++ pruebaBidemnsional.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arreglo_bi = np.array([[1,2,3],[4,5,6]])
+# 1 2 3
+# 4 5 6
+for i in range(2):
+    for j in range(3):
+        print(arreglo_bi[i][j])
+        
+print(arreglo_bi[1][1])
